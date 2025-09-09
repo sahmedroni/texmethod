59c48cdd6dbebdd7cdf9d39423dd2e9c0989c51b
Salahuddin Ahmed <roni.dboy@gmail.com>
2025-09-09 05:32:00 +0000

    undo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center justify-center gap-2">
       <Image 
-        src="https://drive.google.com/uc?export=view&id=1x4SErqdu87_rwj4ZsX0N470TQ1nTivxd" 
+        src="https://drive.google.com/uc?export=view&id=1z00lCKXuyQnXZTbdOkJpcYuQdvflvUy1" 
         alt="Tex Method Logo" 
         width={150} 
         height={50}
