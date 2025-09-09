b7cfcadb0e5be7c9f8d7e49af92b5225b5a9fff2
Salahuddin Ahmed <roni.dboy@gmail.com>
2025-09-09 05:31:44 +0000

    replace the image link with this new link "https://drive.google.com/uc?e

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center justify-center gap-2">
       <Image 
-        src="https://drive.google.com/uc?export=view&id=1z00lCKXuyQnXZTbdOkJpcYuQdvflvUy1" 
+        src="https://drive.google.com/uc?export=view&id=1x4SErqdu87_rwj4ZsX0N470TQ1nTivxd" 
         alt="Tex Method Logo" 
         width={150} 
         height={50}
